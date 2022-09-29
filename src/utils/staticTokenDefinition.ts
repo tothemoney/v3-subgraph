e210f124e12b69496990cc8466d5aafc2239459c
alexconsensusfm <95760500+alexconsensusfm@users.noreply.github.com>
2022-09-29 16:28:05 +0300

    Update staticTokenDefinition.ts

--- src/utils/staticTokenDefinition.ts
+++ src/utils/staticTokenDefinition.ts
@@ -22,59 +22,14 @@ export class StaticTokenDefinition {
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
     let staticDefinitions = new Array<StaticTokenDefinition>(6)
 
-    // Add DGD
-    let tokenDGD = new StaticTokenDefinition(
-      Address.fromString('0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'),
-      'DGD',
-      'DGD',
-      BigInt.fromI32(9)
-    )
-    staticDefinitions.push(tokenDGD)
-
-    // Add AAVE
-    let tokenAAVE = new StaticTokenDefinition(
-      Address.fromString('0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'),
-      'AAVE',
-      'Aave Token',
+    // Add SMT
+    let tokenSMT = new StaticTokenDefinition(
+      Address.fromString('0xb23617F1478f7Cf925a5a35De9cde20b0976526b'),
+      'SMT',
+      'SampleToken',
       BigInt.fromI32(18)
     )
-    staticDefinitions.push(tokenAAVE)
-
-    // Add LIF
-    let tokenLIF = new StaticTokenDefinition(
-      Address.fromString('0xeb9951021698b42e4399f9cbb6267aa35f82d59d'),
-      'LIF',
-      'Lif',
-      BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenLIF)
-
-    // Add SVD
-    let tokenSVD = new StaticTokenDefinition(
-      Address.fromString('0xbdeb4b83251fb146687fa19d1c660f99411eefe3'),
-      'SVD',
-      'savedroid',
-      BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenSVD)
-
-    // Add TheDAO
-    let tokenTheDAO = new StaticTokenDefinition(
-      Address.fromString('0xbb9bc244d798123fde783fcc1c72d3bb8c189413'),
-      'TheDAO',
-      'TheDAO',
-      BigInt.fromI32(16)
-    )
-    staticDefinitions.push(tokenTheDAO)
-
-    // Add HPB
-    let tokenHPB = new StaticTokenDefinition(
-      Address.fromString('0x38c6a68304cdefb9bec48bbfaaba5c5b47818bb2'),
-      'HPB',
-      'HPBCoin',
-      BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenHPB)
+    staticDefinitions.push(tokenSMT)
 
     return staticDefinitions
   }
